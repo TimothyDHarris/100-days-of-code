12a9929beed4bff44cb438895911b547dde3f08b
Timothy Harris <timdavidharris@gmail.com>
2021-01-01 18:31:24 -0700

    This is the first real exercise where I practice getting specific text printed on 3 different lines

---
+++ day-1-1-exercise.py
@@ -0,0 +1,6 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# can use \n between what we want to print to put it on different lines too; see below
+# print("Day 1 - Python Print Function\nThe Function is declared like this:\nprint('what to print;)")

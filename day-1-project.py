880eb22645baa1bbed13362cb2a5ad8870e41d10
Timothy Harris <timdavidharris@gmail.com>
2021-01-02 15:45:50 -0700

    I took the band name generator project and tweaked it to be a short Mad Lib instead as I thought that was more fun.

---
+++ day-1-project.py
@@ -0,0 +1,13 @@
+print("Here is a shot Mad Lib, please fill out these 3 prompts to complete the story.")
+noun1 = input("Give me a noun: ")
+adjective = input("Give me an adjective: ")
+noun2 = input("Give me a second noun: ")
+
+print("Mad Lib: The Mona Lisa")
+print("After hiding the painting in his " + noun1 + " for two years,") 
+print("he grew " + adjective + " and tried to sell it to a/an " + noun2 + " in Florence, but was caught.")
+
+
+# Here is the full mad lib:
+# After hiding the painting in his [noun] for two years, 
+# he grew [adjective] and tried to sell it to a/an [noun] in Florence, but was caught.

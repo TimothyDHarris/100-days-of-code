a220fd4da22c2e6e982c1c50ad71c711750b1c0d
Timothy Harris <timdavidharris@gmail.com>
2021-01-05 19:24:14 -0700

    I took more notes and play with converting variable into different kinds of data

--- day-2-start.py
+++ day-2-start.py
@@ -8,8 +8,18 @@ print("Hello"[4])
 # To declare an integer, just type out an integer (number)
 # You can use underscores to better read large numbers and the computer will ignore them
 # the plus sign works to add the intigers
-print(123 + 456) 
+addition = print(123 + 456) 
 
 # The Float type uses decimal points, like pi
 
 # Boolean is only True or False
+# ----------------------------------------------------------------------------------------------------------
+
+# You can change data types. For example you can change an intiger into a string by using this function below
+new_addition = str(addition)
+print(type(new_addition))
+# You can use the type function to tell you what kind of variable you are working with
+print(type("Hello"))
+# we can also convert something within the print
+print(70 + float("100.5"))
+# you can use str() int() or float() to convert your variables

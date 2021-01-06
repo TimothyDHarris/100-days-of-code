0d586098c79b8b782d029f1b97f0248d49ad9b9b
Timothy Harris <timdavidharris@gmail.com>
2021-01-05 19:44:06 -0700

    I added in the solution the teacher presented, but commented out

--- day-2-1-exercise.py
+++ day-2-1-exercise.py
@@ -7,3 +7,10 @@ two_digit_number = input("Type a two digit number: ")
 first_int = int(two_digit_number[0])
 second_int = int(two_digit_number[1])
 print(first_int + second_int)
+
+####################################
+# Teacher's solution below
+# first_digit = two_digit_number[0]
+# second_digit = two_digit_number[1]
+# result = int(first_digit) + int(second_digit)
+# print(result)

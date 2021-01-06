4e3e4cf78a60c6f57d2ec5b94d571f9585190cc2
Timothy Harris <timdavidharris@gmail.com>
2021-01-05 19:09:27 -0700

    working on a short video discussing how to work with strings, integers, floats, and booleans

---
+++ day-2-start.py
@@ -0,0 +1,15 @@
+# Notes on Strings, Integers, Floats, and Booleans
+
+# The number in the square brackets will determine what gets printed
+# These are strings
+print("Hello"[0])
+print("Hello"[4])
+
+# To declare an integer, just type out an integer (number)
+# You can use underscores to better read large numbers and the computer will ignore them
+# the plus sign works to add the intigers
+print(123 + 456) 
+
+# The Float type uses decimal points, like pi
+
+# Boolean is only True or False

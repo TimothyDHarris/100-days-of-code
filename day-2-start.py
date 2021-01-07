0b50f30eb01abeb57d34414b44dae48a8df23964
Timothy Harris <timdavidharris@gmail.com>
2021-01-06 17:57:50 -0700

    notes about how to round and using f-String

--- day-2-start.py
+++ day-2-start.py
@@ -8,7 +8,7 @@ print("Hello"[4])
 # To declare an integer, just type out an integer (number)
 # You can use underscores to better read large numbers and the computer will ignore them
 # the plus sign works to add the intigers
-addition = print(123 + 456) 
+addition = print(123 + 456)
 
 # The Float type uses decimal points, like pi
 
@@ -26,13 +26,26 @@ print(70 + float("100.5"))
 ##################################################
 
 # how to do math in python
-# addition 
-print(5 + 5)
-# subtraction 
-print(7 - 4)
-# multiplication 
-print(3 * 2)
+# addition
+print("addition", 5 + 5)
+# subtraction
+print("subtraction", 7 - 4)
+# multiplication
+print("multiplication", 3 * 2)
 # division
-print(6 / 3)
+print("division", 6 / 3)
 # raise to the power of
-print(2 ** 3)
+print("raise 2 to the power of (3)", 2 ** 3)
+# round
+print("round the value of this division", round(8 / 3))
+# turn it into an integer without rounding when dividing
+print("turn a division result into an integer", 8 // 3)
+# you can also change a value by adding the math function to the beginning of the equals sign
+score = 0
+# this adds 1 to the value of score
+score += 1
+print("The score is", score)
+
+##################################################
+# f-String
+print(f"you can mix and match variables by adding them within braces here with an 'f' at the beginning with no spaces")

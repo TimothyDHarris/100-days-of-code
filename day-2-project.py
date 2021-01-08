951fd9dbcb0d0e7899299246e4f17b4c28ed0788
Timothy Harris <timdavidharris@gmail.com>
2021-01-07 20:45:39 -0700

    added formatting so that it always returns two decimal places.

--- day-2-project.py
+++ day-2-project.py
@@ -11,5 +11,6 @@ how_many_ways = input("How many people are splitting this bill? ")
 tip_float = float(tip) / 100 + 1.00
 
 calculation = (float(bill_total) / float(how_many_ways)) * tip_float
+calculation_two_decimals = "{:.2f}".format(calculation)
 
-print(f"Each person sould pay: $ {round(calculation, 2)}")
+print(f"Each person sould pay: $ {calculation_two_decimals}")

d57e686cfd8fc705329442ec85e698461ab77ae4
Timothy Harris <timdavidharris@gmail.com>
2021-01-10 20:25:26 -0700

    Adding a nested if statement and an elif statement.

--- day-3-start.py
+++ day-3-start.py
@@ -3,7 +3,16 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
+# nested if else statement
 if height >= 120:
     print("You can ride the rollercoaster!")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("Please pay $5.")
+        # elif statement, use if we have something between an if and else statement
+    elif age <= 18:
+        print("Please pay $7")
+    else:
+        print("Please pay $12.")
 else:
-    print("Sorry, you have to grow taller before you can ride")
+    print("Sorry, you have to grow taller before you can ride!")

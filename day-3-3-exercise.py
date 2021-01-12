f21760f8d1daeadc7aa8801319667f567b1fd7e0
Timothy Harris <timdavidharris@gmail.com>
2021-01-11 20:15:33 -0700

    Leap Year checker: my solution was not roboust enough and this is the solution I got from the video.

--- day-3-3-exercise.py
+++ day-3-3-exercise.py
@@ -2,9 +2,22 @@
 year = int(input("Which year do you want to check? "))
 # 🚨 Don't change the code above 👆
 #Write your code below this line 👇
-if (year % 4 == 0):
-    print(f"The year {year} is a leap year.")
-elif (year % 100 != 0 and year % 400 == 0):
-    print(f"The year {year} is a leap year.")
+# Below is my first attempt at coding for testing for a new year. However, my logic was not interconnected enough and the check was not roboust enough.
+# if (year % 4 == 0):
+#     print(f"The year {year} is a leap year.")
+# elif (year % 100 != 0 and year % 400 == 0):
+#     print(f"The year {year} is a leap year.")
+# else:
+#     print(f"The year {year} is not a leap year.")
+
+# Teacher's solution below with some edits from me
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(f"The year {year} is a leap year.")
+        else:
+            print(f"The year {year} is not a leap year.")
+    else: 
+        print(f"The year {year} is a leap year.")
 else:
     print(f"The year {year} is not a leap year.")

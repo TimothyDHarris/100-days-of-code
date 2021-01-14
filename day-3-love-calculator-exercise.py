81bc98440548ff7be47e2cd1717d79f01e060ecf
Timothy Harris <timdavidharris@gmail.com>
2021-01-13 19:56:30 -0700

    tweaked the initial lines

--- day-3-love-calculator-exercise.py
+++ day-3-love-calculator-exercise.py
@@ -1,6 +1,8 @@
 # 🚨 Don't change the code below 👇
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
-name2 = input("What is their name? \n")
+name2 = input("What is the name of the person you like? \n")
 # 🚨 Don't change the code above 👆
 #Write your code below this line 👇
+string(name1.lower()) = string(name1_lower_case)
+print(name1)

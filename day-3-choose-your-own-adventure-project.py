972ad8fbb7544e569e1008a8b4df41c77ebc2374
Timothy Harris <timdavidharris@gmail.com>
2021-01-16 16:46:52 -0700

    Treasure Island: starter code.

---
+++ day-3-choose-your-own-adventure-project.py
@@ -0,0 +1,11 @@
+print('''
+("`-''-/").___..--''"`-._ 
+ `6_ 6  )   `-.  (     ).`-.__.`) 
+ (_Y_.)'  ._   )  `._ `. ``-..-' 
+   _..`--'_..-_/  /--'_.'
+  ((((.-''  ((((.'  (((.-' 
+  ''')
+print("Welcome to a Rocky Mountain Hike Adventure")
+print("Your mission is to escape the mountain lion")
+# decision flow chart below
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload

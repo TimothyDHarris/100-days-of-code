02c3532c559e895faf67cbd3fd44fd2312960bf4
Timothy Harris <timdavidharris@gmail.com>
2021-01-30 18:52:16 -0700

    my notes for the day 4 material

---
+++ day-4-notes.py
@@ -0,0 +1,10 @@
+# below is how we can import something into our code.
+# We can do this with things from the python team or our other files.
+import random
+
+# randint(a, b) allows us to get a psuedorandom number that is in integer
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+random_float = random.random() * 5
+print(random_float)

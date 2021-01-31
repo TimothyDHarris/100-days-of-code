a110a7cf14984133b2db5c6de462ebaecd05b372
Timothy Harris <timdavidharris@gmail.com>
2021-01-31 14:57:11 -0700

    My solution to this challenge.

--- day-4-random-person-bill-pay.py
+++ day-4-random-person-bill-pay.py
@@ -1,5 +1,9 @@
 # Split string method
-names_string = input("Give me everybody's names, separated by a comma. ")
+names_string = input("Give me everybody's names, separated by a comma.\n")
 names = names_string.split(", ")
 # 🚨 Don't change the code above 👆
-#Write your code below this line 👇
+# #Write your code below this line 👇
+# Note, I am not allowed to use the choice(function)
+import random
+random.shuffle(names)
+print(f"{names[0]} is going to buy the meal today")

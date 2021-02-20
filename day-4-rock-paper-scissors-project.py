e56e7f506ef334ee81fe603120a55a206189bbd3
Timothy Harris <timdavidharris@gmail.com>
2021-02-20 16:55:43 -0700

    my solution to the rock paper scissors project

--- day-4-rock-paper-scissors-project.py
+++ day-4-rock-paper-scissors-project.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -25,3 +27,29 @@ scissors = '''
 ---.__(___)
 '''
 
+rockPaperScissorsList = [rock, paper, scissors]
+NPC_Choice = random.randint(0, 2)
+Person_Choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
+
+print(rockPaperScissorsList[Person_Choice])
+if Person_Choice == NPC_Choice:
+    print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
+    print("You tie")
+elif Person_Choice == 0 and NPC_Choice == 1:
+    print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
+    print("You lose")
+elif Person_Choice == 0 and NPC_Choice == 2:
+    print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
+    print("You win")
+elif Person_Choice == 1 and NPC_Choice == 0:
+    print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
+    print("You win")
+elif Person_Choice == 1 and NPC_Choice == 2:
+    print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
+    print("You lose")
+elif Person_Choice == 2 and NPC_Choice == 0:
+    print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
+    print("You lose")
+elif Person_Choice == 2 and NPC_Choice == 1:
+    print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
+    print("You win")

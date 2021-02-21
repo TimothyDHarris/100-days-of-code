4ef6140e6f4a3a0ada5af64122e4ddb53ee03819
Timothy Harris <timdavidharris@gmail.com>
2021-02-20 17:33:37 -0700

    I added a few things after watching the solution video

--- day-4-rock-paper-scissors-project.py
+++ day-4-rock-paper-scissors-project.py
@@ -31,25 +31,33 @@ rockPaperScissorsList = [rock, paper, scissors]
 NPC_Choice = random.randint(0, 2)
 Person_Choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
 
-print(rockPaperScissorsList[Person_Choice])
-if Person_Choice == NPC_Choice:
+if Person_Choice >= 3 or Person_Choice < 0:
+    print("Sorry, I didn't understand your input. Please try again")
+elif Person_Choice == NPC_Choice:
+    print(rockPaperScissorsList[Person_Choice])
     print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
     print("You tie")
 elif Person_Choice == 0 and NPC_Choice == 1:
+    print(rockPaperScissorsList[Person_Choice])
     print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
     print("You lose")
 elif Person_Choice == 0 and NPC_Choice == 2:
+    print(rockPaperScissorsList[Person_Choice])
     print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
-    print("You win")
+    print("You win!")
 elif Person_Choice == 1 and NPC_Choice == 0:
+    print(rockPaperScissorsList[Person_Choice])
     print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
-    print("You win")
+    print("You win!")
 elif Person_Choice == 1 and NPC_Choice == 2:
+    print(rockPaperScissorsList[Person_Choice])
     print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
     print("You lose")
 elif Person_Choice == 2 and NPC_Choice == 0:
+    print(rockPaperScissorsList[Person_Choice])
     print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
     print("You lose")
 elif Person_Choice == 2 and NPC_Choice == 1:
+    print(rockPaperScissorsList[Person_Choice])
     print(f"Computer chose:\n{rockPaperScissorsList[NPC_Choice]}")
-    print("You win")
+    print("You win!")
